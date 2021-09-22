df1f57530201bc079c05e3a46433f48c24f85288
Lucas Basualdo <basualdo1995@gmail.com>
2021-09-21 22:23:32 -0300

    server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const fs= require('fs');
 
-app.get('/hacker' , (req,res) => {
-    res.send('ahre');
+// app.get('/hacker' , (req,res) => {
+//     res.send('ahre');
+// })
+
+let rawdata = fs.readFileSync('scripts/comida.json');
+let student = JSON.parse(rawdata);
+
+app.get('/api' , (req,res) => {
+    res.send(student)
 })
+
+//  app.get('/search' ,(req,res) => {
+
+//  })
 
 
 app.listen(process.env.PORT || 3000, () => {
