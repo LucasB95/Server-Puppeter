0b7ab059507a587b093bd1e87e51803a8362097a
Lucas Basualdo <basualdo1995@gmail.com>
2021-09-26 02:06:34 -0300

    cambio en el bucle de busqueda2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const busq = require('./server');
         var contador = 1;
 
 
-        for(let i=1 ; i<= 30;i++){
+        for(let i=1 ; i<= 10;i++){
            // while(contador == 1){
 
                 for(const website of websites){
